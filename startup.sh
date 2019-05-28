9f1d75c1676d8990621fe8a4ddeecb9c80a14d4d
Loïc Combis <combis.loic@gmail.com>
2019-05-28 16:59:18 +0200

    [Fix]

--- startup.sh
+++ startup.sh
@@ -1,12 +1,4 @@
 #! /bin/sh
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
-git checkout master
-git pull origin master
-
-mvn --version
-mvn package -Dmaven.test.ski=true
 sudo docker-compose rm webserver
 sudo docker-compose rm database
 
